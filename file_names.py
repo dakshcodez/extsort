fa4f6b2f1f03d2acb248e9464865ce291a252055
dakshcodez <daksharoracodez@gmail.com>
2025-06-10 14:24:54 +0530

    initial commit

---
+++ file_names.py
@@ -0,0 +1,85 @@
+hash_map = {
+    # Text & Code
+    ".txt": "text_file",
+    ".md": "markdown",
+    ".json": "json",
+    ".xml": "xml",
+    ".yaml": "yaml",
+    ".yml": "yaml",
+    
+    # Programming Languages
+    ".py": "python",
+    ".c": "c",
+    ".cpp": "cpp",
+    ".java": "java",
+    ".js": "javascript",
+    ".ts": "typescript",
+    ".rb": "ruby",
+    ".go": "go",
+    ".php": "php",
+    ".swift": "swift",
+    ".kt": "kotlin",
+    ".rs": "rust",
+    ".scala": "scala",
+    ".pl": "perl",
+    ".sql": "sql",
+    ".r": "r",
+    ".dart": "dart",
+    ".erl": "erlang",
+    ".hs": "haskell",
+    ".sh": "shell_script",
+    ".bat": "batch_file",
+
+    # Web
+    ".html": "html",
+    ".css": "css",
+
+    # Data
+    ".csv": "csv",
+    ".tsv": "tsv",
+    ".xls": "excel",
+    ".xlsx": "excel",
+
+    # Documents
+    ".pdf": "pdf",
+    ".doc": "word",
+    ".docx": "word",
+    ".ppt": "powerpoint",
+    ".pptx": "powerpoint",
+    
+    # Audio
+    ".mp3": "audio",
+    ".wav": "audio",
+    ".flac": "audio",
+    ".ogg": "audio",
+    
+    # Video
+    ".mp4": "video",
+    ".mkv": "video",
+    ".mov": "video",
+    ".avi": "video",
+    ".webm": "video",
+
+    # Images
+    ".jpg": "images",
+    ".jpeg": "images",
+    ".png": "images",
+    ".gif": "images",
+    ".bmp": "images",
+    ".svg": "images",
+    ".webp": "images",
+
+    # Archives
+    ".zip": "archives",
+    ".tar": "archives",
+    ".gz": "archives",
+    ".rar": "archives",
+    ".7z": "archives",
+    ".iso": "disk_images",
+
+    # Others
+    ".exe": "executables",
+    ".apk": "android_packages",
+    ".deb": "debian_packages",
+    ".appimage": "linux_packages"
+}
